234add57fc62d909530774b65f8f29e3b3602ed3
Sávio <rodriguessavio68@gmail.com>
2025-04-25 00:25:14 -0300

    first-version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function enviarWhatsApp(produto) {
     const mensagem = `Olá! Tenho interesse no produto "${produto}" que vi no site.`;
-    const numero = '5598999999999'; // substitua pelo número real
+    const numero = '55838217-2222';
     const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
     window.open(link, '_blank');
   }
